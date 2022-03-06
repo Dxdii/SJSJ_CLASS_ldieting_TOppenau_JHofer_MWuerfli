e2c5bd9bcccde9341847bf1899a41a7e66ebc2f4
Dxdii <lukas.dietinger@gmail.com>
2022-03-06 11:51:42 +0100

    Countdown update

--- src/Countdown.java
+++ src/Countdown.java
@@ -2,23 +2,28 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 
-public class Countdown {
+public class Countdown extends Thread{
     public static int countdownStarter = 20;
-
+public boolean frunning = true;
     public Countdown() {
-        Timer timer = new Timer();
-        TimerTask task = new TimerTask() {
-            @Override
-            public void run() {
+        countdownStarter = 20;
+    }
+    @Override
+    public void run() {
+        while(frunning){
+        while (countdownStarter  != 0) {
+            try {
+                Thread.sleep(1000);
+              countdownStarter--;
                 System.out.println(countdownStarter);
-                if (countdownStarter > 0)
-                    countdownStarter--;
 
-                if (countdownStarter == 0)
-                    System.exit(0);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        };
-        timer.schedule(task, 0, 1000);
+        }
+        frunning =false;
+
+    }
     }
 
 }
