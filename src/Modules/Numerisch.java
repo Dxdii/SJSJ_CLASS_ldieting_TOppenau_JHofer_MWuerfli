303032d9172737278a06c981123d919d084c0abe
jhofer02 <jhofer@htl-steyr.ac.at>
2022-03-22 11:29:28 +0100

    change connection

--- src/Modules/Numerisch.java
+++ src/Modules/Numerisch.java
@@ -4,11 +4,11 @@ import Modules.Frage;
 
 public class Numerisch extends Frage {
     public float d;
-    public Numerisch(String s) {
-        super(s);
+    public Numerisch(String s, int knnmmr) {
+        super(s, knnmmr);
     }
-    public Numerisch(String b, float s){
-        super(b);
+    public Numerisch(String b, int knnmmr, float s){
+        super(b, knnmmr);
         d =s;
     }
 }
--- src/Modules/SaveToDatabase.java
+++ src/Modules/SaveToDatabase.java
@@ -8,19 +8,37 @@ import Auswerten.ConnectDatabaseAuswerten;
 import com.sun.jdi.IntegerValue;
 
 import java.sql.*;
+import java.util.Vector;
 
 public class SaveToDatabase {
-    ConnectDatabaseAuswerten a = new ConnectDatabaseAuswerten();
-    Connection db = a.getConnectToDatabase();
+    static ConnectDatabaseAuswerten a = new ConnectDatabaseAuswerten();
+    static Connection db = a.getConnectToDatabase();
 
+    public SaveToDatabase(Vector<Frage> qu) throws SQLException {
+        int j = 0;
+        qu.get(j).text.split(":");
+        Statement sttmnt = db.createStatement();
+        String str = "SELECT count(kennummer)\n" + "FROM frage";
+        PreparedStatement p = db.prepareStatement(str);
+        ResultSet rs = p.executeQuery();
+        System.out.println(rs);
+        rs.next();
+        int i = rs.getInt("count");
+        ++i;
+        sttmnt.executeUpdate("INSERT INTO antwort values (" + i + qu + min + max + typ + ")");
+        System.out.println("INSERT INTO antwort values (" + i + qu + min + max + typ + ")");
+    }
     public SaveToDatabase(JaNein awnser) throws SQLException {
         Statement sttmnt = db.createStatement();
         String str = "SELECT count(kennummer)\n" + "FROM antwort";
         PreparedStatement p = db.prepareStatement(str);
         ResultSet rs = p.executeQuery();
-        int i = rs.getInt("kennummer");
+        System.out.println(rs);
+        rs.next();
+        int i = rs.getInt("count");
         ++i;
-        sttmnt.executeUpdate("INSERT INTO antwort values (" + awnser.Kennummer + i + ")");
+        sttmnt.executeUpdate("INSERT INTO antwort values (" + awnser.Kennummer + ", " + i + ")");
+        System.out.println("INSERT INTO ajanein values (" + i + awnser.value + ")");
         sttmnt.executeUpdate("INSERT INTO ajanein values (" + i + awnser.value + ")");
     }
     public SaveToDatabase(Numerisch awnser){
@@ -28,8 +46,19 @@ public class SaveToDatabase {
     public SaveToDatabase(vonBis awnser) {
 
         }
+
+
     public static void main(String[] args) throws SQLException {
-        JaNein j1 = new JaNein("Frage X", true);
+        Statement sttmnt = db.createStatement();
+        String str = "SELECT count(kennummer)\n" + "FROM frage";
+        PreparedStatement p = db.prepareStatement(str);
+        ResultSet rs = p.executeQuery();
+        System.out.println(rs);
+        rs.next();
+        int i = rs.getInt("count");
+        ++i;
+        JaNein j1 = new JaNein("Frage X", i ,true);
+        SaveToDatabase f = new SaveToDatabase("Frage X", 0, 0, 1);
         SaveToDatabase s = new SaveToDatabase(j1);
     }
 
